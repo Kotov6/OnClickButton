3bfde277b3b32ee4db57c3399b036ba43b727631
Kotov6 <asbatiy@gmail.com>
2016-04-12 00:25:47 +0300

    Activity как обработчик, версия падает

--- app/src/main/java/com/example/android/onclickbutton/MainActivity.java
+++ app/src/main/java/com/example/android/onclickbutton/MainActivity.java
@@ -1,12 +1,12 @@
 package com.example.android.onclickbutton;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements View.OnClickListener {
 
     TextView tvOut;
     Button btnOk;
@@ -18,28 +18,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btnOk.setOnClickListener(this);
+        btnCancel.setOnClickListener(this);
+
         // найдем View-элементы
         tvOut = (TextView) findViewById(R.id.tvOut);
         btnOk = (Button) findViewById(R.id.btnOk);
         btnCancel = (Button) findViewById(R.id.btnCancel);
-
-        View.OnClickListener oclBtn = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // по id определеяем кнопку, вызвавшую этот обработчик
-                switch (v.getId()) {
-                    case R.id.btnOk:
-                        // кнопка ОК
-                        tvOut.setText("Нажата кнопка ОК");
-                        break;
-                    case R.id.btnCancel:
-                        // кнопка Cancel
-                        tvOut.setText("Нажата кнопка Cancel");
-                        break;
-                }
-            }
-        };
-        btnOk.setOnClickListener(oclBtn);
-        btnCancel.setOnClickListener(oclBtn);
     }
+
+    @Override
+    public void onClick(View v) {
+// по id определеяем кнопку, вызвавшую этот обработчик
+        switch (v.getId()) {
+            case R.id.btnOk:
+                // кнопка ОК
+                tvOut.setText("Нажата кнопка ОК");
+                break;
+            case R.id.btnCancel:
+                // кнопка Cancel
+                tvOut.setText("Нажата кнопка Cancel");
+                break;
+        }
+    }
+
 }
